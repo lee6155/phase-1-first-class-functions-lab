eca861873168af76cbc0f2f3ba3e080db2f30dbd
Evan Lee <evan.lee.615@gmail.com>
2024-02-09 23:36:25 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function (array) {
+    return [array[0],array[1]];
+}
+
+const returnLastTwoDrivers = function (array) {
+    return [array[array.length-2],array[array.length-1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function (integer) {
+    return function (fare) {return integer * fare};
+}
+
+const fareDoubler = function (fare) {
+    return fare * 2;
+}
+
+const fareTripler = function (fare) {
+    return fare * 3;
+}
+
+const selectDifferentDrivers = function (array2,functionParameter) {
+    return functionParameter(array2);
+}
